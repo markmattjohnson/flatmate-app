33025fcbdb99b4a323193e26a560068b7e10526a
markjohnson <markderjohnson@aol.com>
2019-05-31 10:45:57 +0200

    deleted cloudinary api because image upload makes more sense next week

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Todos from "./Todos";
 import Header from "./components/Header";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
-import axios from "axios";
-
-const CLOUDNAME = process.env.REACT_APP_CLOUDINARY_dv7mukeaf;
-const PRESET = process.env.REACT_APP_CLOUDINARY_y6umlgqb;
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -31,41 +27,11 @@ const Grid = styled.div`
 const Main = styled.main``;
 
 function App() {
-  const [image, setImage] = useState("");
-
-  function upload(event) {
-    const url = `https://api.cloudinary.com/v1_1/${CLOUDNAME}/upload`;
-
-    const formData = new FormData();
-    formData.append("file", event.target.files[0]);
-    formData.append("upload_preset", PRESET);
-
-    axios
-      .post(url, formData, {
-        headers: {
-          "Content-type": "multipart/form-data"
-        }
-      })
-      .then(onImageSave)
-      .catch(err => console.error(err));
-  }
-
-  function onImageSave(response) {
-    setImage(response.data.url);
-  }
-
   return (
     <Grid>
       <BrowserRouter>
         <GlobalStyles />
         <Header />
-        <div>
-          {image ? (
-            <img src={image} alt="" style={{ width: "100%" }} />
-          ) : (
-            <input type="file" name="file" onChange={upload} />
-          )}
-        </div>
         <Main>
           <Switch>
             <Route exact path="/" component={Shoppingcarts} />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,8 +4,13 @@ import Card from "./Card";
 const Cards = props => {
   return (
     <div>
-      <Card title="Zuletzt verwendet" clickHandler={props.clickHandler} />
-      <Card title="Obst & Gemüse" />
+      <Card title="Obst & Gemüse" clickHandler={props.clickHandler} />
+      <Card title="Brot & Gebäck" clickHandler={props.clickHandler} />
+      <Card title="Milch & Käse" clickHandler={props.clickHandler} />
+      <Card title="Fleisch & Fisch" clickHandler={props.clickHandler} />
+      <Card title="Zutaten & Gewürze" clickHandler={props.clickHandler} />
+      <Card title="Snacks & Süßes" clickHandler={props.clickHandler} />
+      <Card title="Haushalt & Gesundheit" clickHandler={props.clickHandler} />
     </div>
   );
 };
