5ecfd1fc37ba14c011e3fe57ab8658220b458471
markjohnson <markderjohnson@aol.com>
2019-06-06 22:45:28 +0200

    added local storage for shoppingcart

--- src/services.js
+++ src/services.js
@@ -2,6 +2,10 @@ export function getTodos() {
   return fetch("/todos").then(res => res.json());
 }
 
+export function getCartItems() {
+  return fetch("/cartItems").then(res => res.json());
+}
+
 export function getFromLocal(name) {
   try {
     return JSON.parse(localStorage.getItem(name));
