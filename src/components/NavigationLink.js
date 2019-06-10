a826a1f2f582ecfa7dd86e042321f793fed10cd9
markjohnson <markderjohnson@aol.com>
2019-06-10 22:45:15 +0200

    changed navbar presentation

--- src/components/NavigationLink.js
+++ src/components/NavigationLink.js
@@ -2,19 +2,17 @@ import styled from "styled-components";
 import { NavLink as RRDNavlink } from "react-router-dom";
 
 const NavLink = styled(RRDNavlink)`
-  background: rgb(64, 165, 219);
-  color: white;
+  background: white;
+  color: rgb(64, 165, 219);
+  border: 1px solid rgb(64, 165, 219);
   border-radius: 5px;
   text-decoration: none;
   margin: 0px 30px 30px 30px;
   padding: 5px;
 
-  &:hover {
-    background: yellow;
-  }
-
   &.active {
-    background: teal;
+    background: rgb(64, 165, 219);
+    color: white;
   }
 `;
 
