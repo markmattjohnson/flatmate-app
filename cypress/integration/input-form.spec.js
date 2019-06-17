a7f386e87161d2d6d97306f21f9a1b7753688219
markjohnson <markderjohnson@aol.com>
2019-06-17 17:12:23 +0200

    acceptance input test

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,6 +1,16 @@
 describe("Input-form", () => {
-  it("focuses input on load", () => {
+  beforeEach(() => {
     cy.visit("https://flatmate2019.herokuapp.com/todos");
+  });
+
+  it("focuses input on load", () => {
     cy.focused().should("have.class", "input");
+  });
+
+  it("accepts input", () => {
+    const typedText = "Buy Milk";
+    cy.get(".input")
+      .type(typedText)
+      .should("have.value", typedText);
   });
 });
