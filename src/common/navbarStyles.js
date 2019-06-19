8bbe0b9acb35d4f9d81a7f511655624b15105cf6
markjohnson <markderjohnson@aol.com>
2019-06-20 00:44:36 +0200

    fixed navbar transparent

--- src/common/navbarStyles.js
+++ src/common/navbarStyles.js
@@ -4,5 +4,9 @@ export const Navigation = styled.footer`
   display: flex;
   justify-content: space-evenly;
   font-family: roboto;
-  background-image: linear-gradient(rgba(255, 255, 255, 0) 0%, #ff0 100%);
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  margin-top: 10px;
+  background-image: linear-gradient(rgba(255, 255, 255, 0) 0%, #ffffff 100%);
 `;
