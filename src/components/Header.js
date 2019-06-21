04092efe74bb0361c284e47a335aeb7831610c43
markjohnson <markderjohnson@aol.com>
2019-06-21 11:43:27 +0200

    code cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,26 +12,6 @@ function Header() {
       <HeaderImage src="/images/headertodos.jpg" alt="work desk" />
     </HeaderContainer>
   );
-
-  // if (
-  //   (
-  //     <BrowserRouter>
-  //       <Route exact path="/" />
-  //     </BrowserRouter>
-  //   )
-  // ) {
-  //   return (
-  //     <HeaderContainer>
-  //       <HeaderImage src="/images/header.jpg" alt="kitchen" />
-  //     </HeaderContainer>
-  //   );
-  // } else {
-  //   return (
-  //     <HeaderContainer>
-  //       <HeaderImage src="/images/headertodos.jpg" alt="work desk" />
-  //     </HeaderContainer>
-  //   );
-  // }
 }
 
 export default Header;
